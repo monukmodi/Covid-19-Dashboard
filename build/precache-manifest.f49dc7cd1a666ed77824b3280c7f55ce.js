5dbe55f9af7e7d64df26f142b71d5c80b306323c
monukr14 <monukr2012@gmail.com>
2020-10-27 12:38:14 +0530

    Build Files

---
+++ build/precache-manifest.f49dc7cd1a666ed77824b3280c7f55ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31ba8cdd58df1db0bd30cf93439a78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c1112c951ac71de6a54",
+    "url": "/static/css/main.c6709d0f.chunk.css"
+  },
+  {
+    "revision": "1f811446ef2deee1b426",
+    "url": "/static/js/2.6a03de31.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.6a03de31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1112c951ac71de6a54",
+    "url": "/static/js/main.102b5bbe.chunk.js"
+  },
+  {
+    "revision": "fe232c0a922f786e57f7",
+    "url": "/static/js/runtime-main.54c62fe3.js"
+  },
+  {
+    "revision": "49e717b97c5bab1fb24f43b5e5383778",
+    "url": "/static/media/coronavirus.49e717b9.jpg"
+  }
+]);
