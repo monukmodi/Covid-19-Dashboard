48fdcc3996fdd844d2bb397ab065fb71b026bdfe
monukr14 <monukr2012@gmail.com>
2020-10-29 13:17:30 +0530

    deploy

---
+++ build/precache-manifest.d9e4274364c6d9c7c6f551854f309ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072fd6505b3b33e684b6b69094e599af",
+    "url": "/Covid-19-Dashboard/index.html"
+  },
+  {
+    "revision": "7c1112c951ac71de6a54",
+    "url": "/Covid-19-Dashboard/static/css/main.c6709d0f.chunk.css"
+  },
+  {
+    "revision": "d1c19473840bb19b0a6c",
+    "url": "/Covid-19-Dashboard/static/js/2.b13291ab.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Dashboard/static/js/2.b13291ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1112c951ac71de6a54",
+    "url": "/Covid-19-Dashboard/static/js/main.102b5bbe.chunk.js"
+  },
+  {
+    "revision": "d8dc0ad7b7c3d0135fc8",
+    "url": "/Covid-19-Dashboard/static/js/runtime-main.cbe27a14.js"
+  },
+  {
+    "revision": "49e717b97c5bab1fb24f43b5e5383778",
+    "url": "/Covid-19-Dashboard/static/media/coronavirus.49e717b9.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f49dc7cd1a666ed77824b3280c7f55ce.js"
+  "/Covid-19-Dashboard/precache-manifest.d9e4274364c6d9c7c6f551854f309ef5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Covid-19-Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
